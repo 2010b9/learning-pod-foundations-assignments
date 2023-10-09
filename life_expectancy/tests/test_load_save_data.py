f881e6b738db4035ecf402bc99407e68438dc2cc
Bruno Vaz <2010b9@gmail.com>
2023-10-09 18:22:32 +0000

    🧪 Unit tests for TSV and ZIP reading strategies

--- life_expectancy/tests/test_load_save_data.py
+++ life_expectancy/tests/test_load_save_data.py
@@ -1,13 +1,24 @@
 """Test for load_data function"""
 from unittest.mock import patch
 import pandas as pd
-from life_expectancy.clean_data.utils.load_save_data import save_data
+from life_expectancy.clean_data.utils.load_save_data import (
+    save_data, load_data, TSVReadStrategy, ZIPReadStrategy
+)
 from . import FIXTURES_DIR
 
 
-def test_load_data(eu_life_expectancy_tsv):
-    """Test the data loading"""
-    assert eu_life_expectancy_tsv.shape == (100, 63)
+def test_load_data_tsv():
+    """Test load TSV data strategy"""
+    assert load_data(
+        FIXTURES_DIR / "eu_life_expectancy_raw.tsv", TSVReadStrategy()
+    ).shape == (100, 63)
+
+
+def test_load_data_zip():
+    """Test load ZIP data strategy"""
+    assert load_data(
+        FIXTURES_DIR / "eurostat_life_expect.zip", ZIPReadStrategy()
+    ).shape == (474382, 8)
 
 
 @patch("pandas.DataFrame.to_csv")
