319469d6c8f4dbabf9a9384d811f01d956ffffb6
Bruno Vaz <2010b9@gmail.com>
2023-10-10 07:08:47 +0000

    🔁 Change country `str` for `Country` enum

--- life_expectancy/clean_data/main.py
+++ life_expectancy/clean_data/main.py
@@ -5,6 +5,7 @@ from life_expectancy.clean_data.utils.cleaning import clean_data
 from life_expectancy.clean_data.utils.load_save_data import (
     load_data, TSVReadStrategy
 )
+from life_expectancy.clean_data.utils.countries import Country
 
 
 CURRENT_DIR = str(Path(__file__).parents[1])
@@ -12,7 +13,7 @@ LIFE_EXPECTANCY_DATA_PATH = CURRENT_DIR + "/data/eu_life_expectancy_raw.tsv"
 
 
 def main(
-    region: str = "PT", path: str = LIFE_EXPECTANCY_DATA_PATH
+    region: Country = Country.PT, path: str = LIFE_EXPECTANCY_DATA_PATH
 ) -> pd.DataFrame:
     """Loads the life expectancy, cleans and saves it as a csv file"""
     life_expectancy_df = load_data(path, TSVReadStrategy())
@@ -22,6 +23,12 @@ def main(
 
 if __name__ == "__main__": # pragma: no cover
     parser = argparse.ArgumentParser()
-    parser.add_argument("--region", default="PT")
-    region_str = parser.parse_args().region
-    main(region_str)
+
+    # Solution based in https://stackoverflow.com/questions/43968006/
+    # support-for-enum-arguments-in-argparse
+    parser.add_argument(
+        "--region", type=lambda country: Country[country],
+        choices=list(Country), default=Country.PT
+    )
+
+    main(parser.parse_args().region)
